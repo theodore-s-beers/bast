5662368f9921bcaabc3103a817bacb99739b0378
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-06-08 23:23:17 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let status = 'qabż';
+	$: statusFa = status === 'basṭ' ? 'بسط' : 'قبض';
+
+	onMount(async () => {
+		const res = await fetch('https://qabz.fly.dev/en');
+		status = await res.text();
+	});
+</script>
+
+<div
+	class="flex h-screen items-end justify-center bg-cover bg-center"
+	class:bg-bast={status === 'basṭ'}
+	class:bg-qabz={status === 'qabż'}
+>
+	<div
+		class="mb-12 rounded-lg bg-gray-100 px-10 py-6 font-arabic text-5xl font-medium"
+		class:pt-4={status === 'qabż'}
+		class:pb-8={status === 'qabż'}
+	>
+		{statusFa}
+	</div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				bast: 'url(bast.jpg)',
+				qabz: 'url(qabz.jpg)'
+			},
+			fontFamily: {
+				arabic: '"Scheherazade New", serif'
+			}
+		}
+	},
+	plugins: []
+};
