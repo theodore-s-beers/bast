07e09a496ee9db3047e6be196d1dd6d24a653c09
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-06-09 18:58:14 +0200

    Adjust layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 	let status = 'qabż';
 	$: statusFa = status === 'basṭ' ? 'بسط' : 'قبض';
 
+	$: offset = status === 'basṭ' ? 'lg:mr-[25vw]' : 'lg:mr-[50vw]';
+
 	onMount(async () => {
 		const res = await fetch('https://qabz.fly.dev/en');
 		status = await res.text();
@@ -11,12 +13,12 @@
 </script>
 
 <div
-	class="flex h-screen items-end justify-center bg-cover bg-center"
+	class="flex h-screen justify-center bg-cover bg-center lg:items-center"
 	class:bg-bast={status === 'basṭ'}
 	class:bg-qabz={status === 'qabż'}
 >
 	<div
-		class="mb-12 rounded-lg bg-gray-100 px-10 py-6 font-arabic text-5xl font-medium"
+		class="mt-8 h-fit rounded-lg bg-gray-100 px-10 py-6 font-arabic text-5xl font-medium {offset}"
 		class:pt-4={status === 'qabż'}
 		class:pb-8={status === 'qabż'}
 	>
