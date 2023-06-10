353a00f9c7321e1cfac79c95b1997eff5166a0af
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2023-06-10 19:27:40 +0200

    Add more meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,8 @@
 
 <svelte:head>
 	<meta name="description" content="A signal of my state of mind" />
+	<meta property="og:image" content="https://www.theobeers.com/bast/og.jpg" />
+	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content="قبض و بسط" />
 	<title>قبض و بسط</title>
 </svelte:head>
