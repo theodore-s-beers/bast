c234e86c24e9c82432ea9c3951d0d30dd7186eb3
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-01-21 14:53:05 -0500

    Format with new Prettier config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount } from "svelte";
 
-	let status = 'qabż';
-	$: statusFa = status === 'basṭ' ? 'بسط' : 'قبض';
+	let status = "qabż";
+	$: statusFa = status === "basṭ" ? "بسط" : "قبض";
 
-	$: offset = status === 'basṭ' ? 'lg:mr-[25vw]' : 'lg:mr-[50vw]';
+	$: offset = status === "basṭ" ? "lg:mr-[25vw]" : "lg:mr-[50vw]";
 
 	onMount(async () => {
-		const res = await fetch('https://qabz.fly.dev/en');
+		const res = await fetch("https://qabz.fly.dev/en");
 		status = await res.text();
 	});
 </script>
@@ -25,13 +25,13 @@
 
 <div
 	class="flex h-screen justify-center bg-cover bg-center lg:items-center"
-	class:bg-bast={status === 'basṭ'}
-	class:bg-qabz={status === 'qabż'}
+	class:bg-bast={status === "basṭ"}
+	class:bg-qabz={status === "qabż"}
 >
 	<div
 		class="mt-8 h-fit rounded-lg bg-gray-100 px-10 py-6 font-arabic text-5xl font-medium {offset}"
-		class:pt-4={status === 'qabż'}
-		class:pb-8={status === 'qabż'}
+		class:pt-4={status === "qabż"}
+		class:pb-8={status === "qabż"}
 	>
 		{statusFa}
 	</div>
